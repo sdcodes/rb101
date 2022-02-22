ae0e4ad1ffa5aec0f85d945a6acfc3315570e1ed
Santi D <santid13@hotmail.com>
2022-02-22 17:09:50 +0000

    contin lesson 2

--- lesson2/rockpaperscissors.rb
+++ lesson2/rockpaperscissors.rb
@@ -2,50 +2,46 @@ VALID_CHOICES = ['rock', 'paper', "scissors"]
 
 def prompt(message)
   puts "=> #{message}"
-end 
+end
 
-def display_result(choice, computer_choice)
-  if (choice == "rock" && computer_choice == "scissors") ||
-      (choice == 'paper' && computer_choice == "rock") || 
-        (choice == "scissors" && computer_choice == "paper")
+def win?(first, second)
+  (first == "rock" && second == "scissors") ||
+    (first == 'paper' && second == "rock") ||
+    (first == "scissors" && second == "paper")
+end
+
+def display_result(player, computer)
+  if win?(player, computer)
     prompt "You won!"
-  elsif (choice == "rock" && computer_choice == "paper") ||
-          (choice == 'paper' && computer_choice == "scissors") || 
-           (choice == "scissors" && computer_choice == "rock")
+  elsif win?(computer, player)
     prompt "Computer won!"
   else
     prompt "It's a tie!"
   end
 end
 
-loop do #main loop start
+loop do # main loop start
   choice = ""
-  loop do 
-  prompt "Choose one: #{VALID_CHOICES.join(', ')}"
-  choice = gets.chomp
-    
+  loop do
+    prompt "Choose one: #{VALID_CHOICES.join(', ')}"
+    choice = gets.chomp
+
     if VALID_CHOICES.include?(choice)
       break
     else
       prompt "That's not a valid choice."
-    end 
-  end 
-  
+    end
+  end
+
   computer_choice = VALID_CHOICES.sample
-  
+
   puts "You chose #{choice}. Computer chose #{computer_choice}."
-  
+
   display_result(choice, computer_choice)
 
-prompt "Do you want to play again?"
+  prompt "Do you want to play again?"
   answer = gets.chomp
-break unless answer.downcase.start_with?("y")
-
-end #end of main loop  
+  break unless answer.downcase.start_with?("y")
+end # end of main loop
 
 prompt "Thank you for playing. Goodbye!"
-
-
-
-
-
