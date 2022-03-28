5804107a28bce28d4957a79f94adc0f205c2dbef
Santi D <santid13@hotmail.com>
2022-03-28 01:21:53 +0000

    working on lesson6

---
+++ lesson6/tictactoe.pseudocode.rb
@@ -0,0 +1,20 @@
+# two player game
+# 3x3 table/board that provides nine placements
+#each player takes a turn marking a placement on the board.
+#in order to win, a player must have 3 marks in a row/column/diangle
+#other player must prevent player from reaching 3 marks while also trying to get 3 marks
+#if no player can get 3 marks, neither wins and the game ends
+
+
+
+1. show the 3x3 board
+2. first player - assigned to user
+3. user marks one of the nine sections
+4. computer chooses a place 
+5. if winner, show winder
+6. if full is full, show tie
+7. if neither, restart loop until 5 or 6 can we executed
+8. ask if player wants to play again?
+9. if yes, press 'y'
+10. if no, exit game
+11. 
---
+++ lesson6/tictactoe.rb
@@ -0,0 +1,107 @@
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
+                  [[1, 5, 9], [3, 5, 7]] # diag
+
+# rubocop:disable Metrics/AbcSize
+def display_board(brd)
+  system 'clear'
+  puts "You're X. Computer is O"
+  puts ""
+  puts "     |     |     "
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}  "
+  puts "     |     |     "
+  puts "-----+-----+-----"
+  puts "     |     |     "
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}  "
+  puts "     |     |     "
+  puts "-----+-----+-----"
+  puts "     |     |     "
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}  "
+  puts "     |     |     "
+  puts ''
+end
+# rubocop:enable Metrics/AbcSize
+
+def initalize_board
+  new_board = {}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
+  new_board
+end
+
+def empty_square(brd)
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+end
+
+def player_places_piece!(brd)
+  square = ''
+  loop do
+    puts "Choose a square(#{empty_square(brd).join(', ')}:"
+    square = gets.chomp.to_i
+    break if empty_square(brd).include?(square)
+    puts "Sorry, that's not a valid choice."
+  end
+  brd[square] = PLAYER_MARKER
+end
+
+def computer_places_piece!(brd)
+  square = empty_square(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+  empty_square(brd).empty?
+end
+
+def someone_won?(brd)
+  !!detect_winner(brd)
+end
+
+def detect_winner(brd)
+   WINNING_LINES.each do |line|
+    # if brd[line[0]] == PLAYER_MARKER &&
+    #   brd[line[1]] == PLAYER_MARKER &&
+    #   brd[line[2]] == PLAYER_MARKER
+    #   return "Player"
+    # elsif brd[line[0]] == COMPUTER_MARKER &&
+    #       brd[line[1]] == COMPUTER_MARKER &&
+    #       brd[line[2]] == COMPUTER_MARKER
+    #   return 'Computer'
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+      return "Player"
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+      return "Computer"
+    end
+  end
+  nil
+end
+
+loop do
+  board = initalize_board
+
+  loop do
+    display_board(board)
+
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
+  display_board(board)
+
+  if someone_won?(board)
+    puts "#{detect_winner(board)} won!"
+  else
+    puts "It's a tie!"
+  end
+
+  puts "Play again? ( y or n)"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+puts "Thanks for playing Tic Tac Toe! Goodbye!"
